00b50a6363c19eaeba9e037c5549f07500d888d1
Nicola Pisanti <nicola@npisanti.com>
2018-12-29 03:30:49 +0100

    improved fetch

--- fetch_data.sh
+++ fetch_data.sh
@@ -3,7 +3,7 @@ echo "clearing destination folder..."
 mkdir -p /home/$USER/htdocs/npisanti-nocms/html_output/data
 rm -rf /home/$USER/htdocs/npisanti-nocms/html_output/data
 echo "getting data..."
-wget -r -np  http://npisanti.com/data/
+wget -r -m --no-parent -erobots=off --reject="index.html*" http://npisanti.com/data/
 echo "moving data..."
 mv npisanti.com/data/ /home/$USER/htdocs/npisanti-nocms/html_output
 echo "clearing..."
